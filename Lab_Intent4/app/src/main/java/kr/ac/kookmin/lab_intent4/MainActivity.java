1d7385752f80ec00fb9609d65913594d48c995a0
K_SSUN <soyen_20@naver.com>
2015-12-12 22:31:28 +0900

    Lab_Intent4 Implicit Intent

---
+++ Lab_Intent4/app/src/main/java/kr/ac/kookmin/lab_intent4/MainActivity.java
@@ -0,0 +1,48 @@
+package kr.ac.kookmin.lab_intent4;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.web).setOnClickListener(mClickListener);
+        findViewById(R.id.dial).setOnClickListener(mClickListener);
+        findViewById(R.id.picture).setOnClickListener(mClickListener);
+    }
+
+    Button.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+            switch(v.getId()) {
+                case R.id.web :
+                    intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"));
+                    startActivity(intent);
+                    break;
+
+                case R.id.dial :
+                    intent  = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:010-3102-1695"));
+                    startActivity(intent);
+
+                case R.id.picture:
+                    intent = new Intent(Intent.ACTION_VIEW);
+                    Uri uri = Uri.fromFile(new File("/sdcard/test.jpg"));
+                    intent.setDataAndType(uri, "image/jpeg");
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
+
+}
+
